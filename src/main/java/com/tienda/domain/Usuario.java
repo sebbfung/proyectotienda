23a7b738482ec4fe359d541c1d3d5db8b917bacb
sebastianfung <sebastianfung@localhost>
2026-07-27 18:18:41 -0600

    COMMIt sebastian fung 11

--- src/main/java/com/tienda/domain/Usuario.java
+++ src/main/java/com/tienda/domain/Usuario.java
@@ -1,8 +1,11 @@
 package com.tienda.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import java.io.Serializable;
 import java.util.Set;
+import java.util.HashSet;
 import lombok.Data;
 
 @Data
@@ -16,13 +19,31 @@ public class Usuario implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_usuario")
     private Integer idUsuario;
+
+    @NotBlank
+    @Column(unique = true, length = 30)
     private String username;
+
+    @Column(length = 512)
     private String password;
+
+    @Column(length = 20)
     private String nombre;
+
+    @Column(length = 30)
+    @NotBlank
     private String apellidos;
+
+    @Column(unique = true, length = 75)
+    @Email
     private String correo;
+
+    @Column(length = 25)
     private String telefono;
+
+    @Column(length = 1024)
     private String rutaImagen;
+
     private boolean activo;
 
     // Relación Many-to-Many con la entidad Rol
@@ -32,5 +53,5 @@ public class Usuario implements Serializable {
             joinColumns = @JoinColumn(name = "id_usuario"),
             inverseJoinColumns = @JoinColumn(name = "id_rol")
     )
-    private Set<Rol> roles;
+    private Set<Rol> roles = new HashSet<>();
 }
